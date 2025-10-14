14d0e8bca6bb72a2ca7fc342bbcaf340c486e1e3
JEAN Maxime <jean.maxime0988@gmail.com>
2025-10-14 14:13:33 +0700

    refac: clear project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [],
+  template: '',
 })
-export class App {
-  protected readonly title = signal('expense-manager-front');
-}
+export class App {}
