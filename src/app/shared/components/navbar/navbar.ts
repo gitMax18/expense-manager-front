0c6d8970d2146d68c72a1e777377fafba93f8f8b
JEAN Maxime <jean.maxime0988@gmail.com>
2025-10-15 18:10:36 +0700

    feat(auth): add logout btn to navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavItem } from '../nav-item/nav-item';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
+import { Button } from 'primeng/button';
+import { AuthService } from '../../../core/auth/auth-service';
 
 @Component({
   selector: 'app-navbar',
   host: {
     class: 'navbar',
   },
-  imports: [NavItem, MenuModule],
+  imports: [NavItem, MenuModule, Button],
   template: `
     <p-menu [model]="items" styleClass="navbar__menu">
       <ng-template #start>
@@ -17,11 +19,24 @@ import { MenuItem } from 'primeng/api';
       <ng-template #item let-item>
         <app-nav-item [item]="item" />
       </ng-template>
-      <ng-template #end> </ng-template>
+      <ng-template #end>
+        <p-button
+          label="Logout"
+          icon="pi pi-sign-out"
+          class="p-button-text"
+          severity="danger"
+          (onClick)="handleLogout()"
+        />
+      </ng-template>
     </p-menu>
   `,
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  authService = inject(AuthService);
   items: MenuItem[] = [{ label: 'Accounts', icon: 'pi pi-fw pi-home', routerLink: '/accounts' }];
+
+  handleLogout() {
+    this.authService.logout();
+  }
 }
