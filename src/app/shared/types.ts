08a2df35ed4d0b2790304208044f8a24b45fb6a9
JEAN Maxime <jean.maxime0988@gmail.com>
2025-10-19 15:50:39 +0700

    feat(http): add httpRequestState service

--- src/app/shared/types.ts
+++ src/app/shared/types.ts
@@ -1,4 +1,31 @@
+import { Signal } from '@angular/core';
+import { Observable } from 'rxjs';
+
 export type SelectOption<TValue> = {
   label: string;
   value: TValue;
+};
+
+export type HttpRequestState<TResponse> = {
+  isLoading: Signal<boolean>;
+  error: Signal<string | null>;
+  data: Signal<TResponse | null>;
+  message: Signal<string | null>;
+  errorDetails: Signal<Record<string, string> | null>;
+  execute: (
+    requestFactory: () => Observable<SuccessResponse<TResponse>>
+  ) => Observable<SuccessResponse<TResponse>>;
+  reset: () => void;
+};
+
+export type SuccessResponse<T> = {
+  success: true;
+  message: string;
+  data: T;
+};
+
+export type ErrorResponse = {
+  success: false;
+  error: string;
+  details: Record<string, string>;
 };
