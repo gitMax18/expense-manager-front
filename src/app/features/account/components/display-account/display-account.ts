228f222734730f9b869afc1f42248a52b7e0cbff
JEAN Maxime <jean.maxime0988@gmail.com>
2025-10-21 11:39:41 +0700

    feat(account): add display account

---
+++ src/app/features/account/components/display-account/display-account.ts
@@ -0,0 +1,75 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { Account } from '../../types';
+
+@Component({
+  selector: 'app-display-account',
+  host: {
+    class: 'display-account',
+  },
+  imports: [CardModule, CurrencyPipe, ButtonModule],
+  template: `
+    <p-card>
+      <ng-template #title>
+        {{ account().name }}
+      </ng-template>
+
+      <ng-template #subtitle>
+        {{ account().type }}
+      </ng-template>
+
+      <div class="display-account__content">
+        @if (account().description) {
+        <p class="display-account__description">
+          {{ account().description }}
+        </p>
+        }
+
+        <div class="display-account__row">
+          <span class="display-account__label">Balance</span>
+          <span class="display-account__value">
+            {{ account().balance | currency : account().currency }}
+          </span>
+        </div>
+
+        <div class="display-account__row">
+          <span class="display-account__label">Currency</span>
+          <span class="display-account__value">{{ account().currency }}</span>
+        </div>
+
+        <div class="display-account__row">
+          <span class="display-account__label">Status</span>
+          <span class="display-account__value">
+            {{ account().archived ? 'Archived' : 'Active' }}
+          </span>
+        </div>
+      </div>
+
+      <div class="display-account__actions">
+        <p-button
+          label="Update"
+          icon="pi pi-pencil"
+          severity="secondary"
+          (onClick)="handleUpdate()"
+        />
+        <p-button label="Delete" icon="pi pi-trash" severity="danger" (onClick)="handleDelete()" />
+      </div>
+    </p-card>
+  `,
+  styleUrl: './display-account.scss',
+})
+export class DisplayAccount {
+  account = input.required<Account>();
+  onUpdate = output<Account>();
+  onDelete = output<string>();
+
+  handleUpdate() {
+    this.onUpdate.emit(this.account());
+  }
+
+  handleDelete() {
+    this.onDelete.emit(this.account().id);
+  }
+}
